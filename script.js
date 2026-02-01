c13d62fd5fd10026e7370c240c63855a26fc725d
marufmahadyarnob <137247420+marufmahadyarnob@users.noreply.github.com>
2026-02-01 19:52:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -26,19 +26,19 @@ function createCard(file) {
 
     const bright = document.createElement("input");
     bright.type = "range"; bright.min = 0.8; bright.max = 1.3; bright.step = 0.05; bright.value = 1;
-    bright.oninput = () => applyBrightness(card, bright.value);
+    bright.oninput = () => img.style.filter = `brightness(${bright.value})`;
 
     const smoothBtn = document.createElement("button");
     smoothBtn.innerText = "Smooth";
-    smoothBtn.onclick = () => applySmooth(card);
+    smoothBtn.onclick = () => img.style.filter = `brightness(${bright.value}) blur(0.6px)`;
 
     const bgBtn = document.createElement("button");
     bgBtn.innerText = "Remove BG";
-    bgBtn.onclick = () => removeBg(card);
+    bgBtn.onclick = () => removeBg(img);
 
     const downBtn = document.createElement("button");
     downBtn.innerText = "Download";
-    downBtn.onclick = () => download(card);
+    downBtn.onclick = () => download(img);
 
     controls.append(cropBtn, bright, smoothBtn, bgBtn, downBtn);
     card.append(img, controls);
@@ -59,45 +59,31 @@ function crop(card) {
 
 function autoCropAll() { cards.forEach(crop); }
 
-function applyBrightness(card, val) {
-  const img = card.querySelector("img");
-  img.style.filter = `brightness(${val})`;
-}
-
-function applySmooth(card) {
-  const img = card.querySelector("img");
-  const prevFilter = img.style.filter || "";
-  img.style.filter = prevFilter + " blur(0.6px)";
-}
-
-/* Portrait BG remove for solid backgrounds */
-function removeBg(card) {
-  const img = card.querySelector("img");
+function removeBg(img) {
   const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d");
-
   canvas.width = img.naturalWidth;
   canvas.height = img.naturalHeight;
+  const ctx = canvas.getContext("2d");
   ctx.drawImage(img, 0, 0);
 
   const data = ctx.getImageData(0, 0, canvas.width, canvas.height);
   const d = data.data;
 
   for (let i = 0; i < d.length; i += 4) {
-    if (d[i] > 200 && d[i+1] > 200 && d[i+2] > 200) { d[i+3] = 0; }
+    if (d[i] > 200 && d[i+1] > 200 && d[i+2] > 200) d[i+3] = 0;
   }
 
   ctx.putImageData(data, 0, 0);
   img.src = canvas.toDataURL();
 }
 
-function removeBgAll() { cards.forEach(c => removeBg(c)); }
+function removeBgAll() { cards.forEach(c => removeBg(c.querySelector("img"))); }
 
-function download(card) {
+function download(img) {
   const a = document.createElement("a");
-  a.href = card.querySelector("img").src;
+  a.href = img.src;
   a.download = "image.png";
   a.click();
 }
 
-function downloadAll() { cards.forEach(download); }
+function downloadAll() { cards.forEach(c => download(c.querySelector("img"))); }
